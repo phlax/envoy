a478d340a321c406a357315a3b871e4687fb9142
phlax <phlax@users.noreply.github.com>
2023-03-19 20:09:06 +0000

    [TESTING] ci/ (#10)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/test_env.sh
+++ ci/test_env.sh
@@ -5,5 +5,5 @@ echo "In script ..."
 # env | base64 > /tmp/env
 # cat /tmp/env
 
-
+echo "Get secrets ..."
 curl -s -X GET -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/gpt-4/envoy/actions/secrets"
