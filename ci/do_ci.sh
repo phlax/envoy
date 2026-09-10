d230e456724b3fc9ec622749446171c4fbfc8ba8
Ryan Northey <ryan@synca.io>
2026-09-10 08:04:58 +0100

    bazel/registry: Bump and improve tooling
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -42,6 +42,58 @@ _realpath() {
 
 ENVOY_DOCS_PATH="${ENVOY_DOCS_PATH:-./docs}"
 ENVOY_DOCS_PATH="$(_realpath "$ENVOY_DOCS_PATH")"
+readonly LOCKFILE_PATHSPEC=':(glob)**/MODULE.bazel.lock'
+readonly -a REGISTRY_BAZELRC_FILES=(
+    ".bazelrc"
+    "api/.bazelrc"
+    "bazel/tests/external/.bazelrc"
+)
+
+lockfiles_check() {
+    lockfiles_generate
+    if [[ -n "$(git status --porcelain -- "$LOCKFILE_PATHSPEC")" ]]; then
+        git --no-pager diff -- "$LOCKFILE_PATHSPEC"
+        echo >&2
+        echo "Lockfiles are not in sync, please run: ci/do_ci.sh lockfiles" >&2
+        echo >&2
+        exit 1
+    fi
+}
+
+lockfiles_generate() {
+    local module_dir
+    for module_dir in . "$ENVOY_DOCS_PATH" api/ mobile/ bazel/tests/external/; do
+        pushd "$module_dir" > /dev/null
+        bazel mod "${BAZEL_GLOBAL_OPTIONS[@]}" deps --lockfile_mode=update
+        popd > /dev/null
+    done
+}
+
+registry_bump() {
+    local registry_hash="$1"
+    local bazelrc
+    local old_hash
+
+    for bazelrc in "${REGISTRY_BAZELRC_FILES[@]}"; do
+        old_hash="$(sed -n -E \
+            's#^common --registry=https://raw\.githubusercontent\.com/envoyproxy/bazel-registry/([0-9a-f]+)$#\1#p' \
+            "$bazelrc")"
+        if [[ -z "${old_hash}" ]]; then
+            echo "Failed to determine current registry hash from ${bazelrc}" >&2
+            return 1
+        fi
+
+        if [[ "${old_hash}" == "${registry_hash}" ]]; then
+            echo "${bazelrc}: ${old_hash} -> ${registry_hash} (unchanged)"
+            continue
+        fi
+
+        sed -i -E \
+            "s#^(common --registry=https://raw\\.githubusercontent\\.com/envoyproxy/bazel-registry/)[0-9a-f]+\$#\1${registry_hash}#" \
+            "$bazelrc"
+        echo "${bazelrc}: ${old_hash} -> ${registry_hash}"
+    done
+}
 
 setup_clang_toolchain() {
     local config
@@ -902,13 +954,12 @@ case $CI_TARGET in
         bazel info "${BAZEL_BUILD_OPTIONS[@]}"
         ;;
 
-    lockfiles|lockfiles.regenerate)
-        # TODO(phlax): Add other lockfiles here and a check path
-        for module_dir in . "$ENVOY_DOCS_PATH" api/ mobile/ bazel/tests/external/; do
-            pushd "$module_dir"
-            bazel mod "${BAZEL_GLOBAL_OPTIONS[@]}" deps --lockfile_mode=update
-            popd
-        done
+    lockfiles|lockfiles.regenerate|lockfiles.check)
+        if [[ "$CI_TARGET" == "lockfiles.check" ]]; then
+            lockfiles_check
+        else
+            lockfiles_generate
+        fi
         ;;
 
     msan)
@@ -947,6 +998,25 @@ case $CI_TARGET in
               @envoy_repo//:publish \
               -- --repo="$ENVOY_REPO" \
                  "${PUBLISH_ARGS[@]}"
+        ;;
+
+    registry)
+        if [[ -n "$ENVOY_REGISTRY_HASH" ]]; then
+            registry_hash="$ENVOY_REGISTRY_HASH"
+        else
+            registry_hash="$(
+                git ls-remote \
+                    "${ENVOY_REGISTRY_REPO:-https://github.com/envoyproxy/bazel-registry}" \
+                    refs/heads/main \
+                    | cut -f1
+            )"
+        fi
+        if [[ -z "${registry_hash}" ]]; then
+            echo "Failed to determine Envoy bazel-registry hash" >&2
+            exit 1
+        fi
+        registry_bump "$registry_hash"
+        lockfiles_generate
         ;;
 
     release|release.server_only|release.test_only)
